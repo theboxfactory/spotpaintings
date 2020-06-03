3cf66fdcc2d7b759696921b2b46a6e59749c041d
Ian Clark <ian@olsen-clark.com>
2020-06-03 09:13:40 -0500

    Basic fade in / fade out working.

--- app.js
+++ app.js
@@ -15,7 +15,14 @@ function update() {
     for(let x=0; x < 50; x++) {
       var color = create_color();
       var style = `background-color: rgb(${color.r}, ${color.g}, ${color.b})`;
-      html += `<span class="circle" style="${style}"></span>`
+      var css_class = '';
+      if (Math.random() > 0.5) {
+        css_class = 'circle_out'; 
+      } 
+      else {
+        css_class = 'circle_in'; 
+      }
+      html += `<span class="${css_class}" style="${style}"></span>`
     }
 
     container.innerHTML = html;
@@ -28,6 +35,6 @@ document.addEventListener('DOMContentLoaded',function() {
   console.log('Height: ' + window.innerHeight);
   update();
 
-  setInterval(update, 1000);
+  setInterval(update, 10000);
   
 }); 
