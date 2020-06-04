8534667b0d85a19dc5a131adb20927e8a3ba2685
Ian Clark <ian@olsen-clark.com>
2020-06-03 21:16:01 -0500

    General code clean-up.

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 // @ts-nocheck
 
+const SPOT_COUNT = 50;
+let spots = [];
+
 function create_color() {
   return {
     r: Math.random() * 256,
@@ -9,32 +12,33 @@ function create_color() {
 }
 
 function update() {
-    var container = document.getElementById('container');
-    var html = '';
+  const container = document.getElementById('container');
+  let html = '';
 
-    for(let x=0; x < 50; x++) {
-      var color = create_color();
-      var style = `background-color: rgb(${color.r}, ${color.g}, ${color.b})`;
-      var css_class = '';
-      if (Math.random() > 0.5) {
-        css_class = 'circle_out'; 
-      } 
-      else {
-        css_class = 'circle_in'; 
-      }
-      html += `<span class="${css_class}" style="${style}"></span>`
+  for (let x = 0; x < SPOT_COUNT; x++) {
+    let color = create_color();
+    let style = `background-color: rgb(${color.r}, ${color.g}, ${color.b})`;
+    let css_class = '';
+
+    if (Math.random() > 0.5) {
+      css_class = 'circle_out';
+    } else {
+      css_class = 'circle_in';
     }
 
-    container.innerHTML = html;
+    html += `<span class="${css_class}" style="${style}"></span>`
+  }
+
+  container.innerHTML = html;
 }
 
 //  Wait for document to load before defining event handlers
-document.addEventListener('DOMContentLoaded',function() {
+document.addEventListener('DOMContentLoaded', function () {
 
-  console.log('Width: ' + window.innerWidth);
-  console.log('Height: ' + window.innerHeight);
+  // console.log('Width: ' + window.innerWidth);
+  // console.log('Height: ' + window.innerHeight);
   update();
 
   setInterval(update, 10000);
-  
-}); 
+
+});
